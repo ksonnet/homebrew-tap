523cd341c5139af0b897ecda0fd9aebf8feec415
bryanl <bryanliles@gmail.com>
2018-03-08 16:47:39 -0500

    ks version v0.9.1
    Signed-off-by: bryanl <bryanliles@gmail.com>

--- ks.rb
+++ ks.rb
@@ -3,7 +3,7 @@ class Ks < Formula
   homepage "https://ksonnet.io"
   url "https://github.com/ksonnet/ksonnet/releases/download/v0.9.1/ks_0.9.1_darwin_amd64.tar.gz"
   version "0.9.1"
-  sha256 "2a2155feb8930c3fc95ac705878d0eeb0af71cdd4a7697291c6d20c9d422a3ee"
+  sha256 "4300cbff2f9401b28d2c0a7fa50eb4402282bfef5ed5ddafe65ac5ddbccbbeab"
 
   def install
     bin.install "ks"
