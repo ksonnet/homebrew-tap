642cdbfdf4a5ec1638c1e005c4eb9544c474cf13
bryanl <bryanliles@gmail.com>
2018-04-26 10:38:59 -0400

    Brew formula update for ks version v0.10.0
    Signed-off-by: bryanl <bryanliles@gmail.com>

--- ks.rb
+++ ks.rb
@@ -1,9 +1,9 @@
 class Ks < Formula
   desc "The ksonnet command line utility. Manage and deploy Kubernetes applications."
   homepage "https://ksonnet.io"
-  url "https://github.com/ksonnet/ksonnet/releases/download/v0.9.2/ks_0.9.2_darwin_amd64.tar.gz"
-  version "0.9.2"
-  sha256 "cf58b841f1953fa00452b2669d4ee0f094950534c366a8be6487916259e9b955"
+  url "https://github.com/ksonnet/ksonnet/releases/download/v0.10.0/ks_0.10.0_darwin_amd64.tar.gz"
+  version "0.10.0"
+  sha256 "8c12c6340daf7fdbe90f99adbcb76f5a0410e8d5c1c2419541121d542bce6105"
 
   def install
     bin.install "ks"
