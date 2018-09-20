aa82a6a06d79b5f2dba49d109882930f54318865
heptibot <heptibot@heptio.com>
2018-09-20 17:18:40 -0400

    Brew formula update for ks version v0.13.0
    Signed-off-by: Oren Shomron <shomron@gmail.com>

--- ks.rb
+++ ks.rb
@@ -3,7 +3,7 @@ class Ks < Formula
   homepage "https://ksonnet.io"
   url "https://github.com/ksonnet/ksonnet/releases/download/v0.13.0/ks_0.13.0_darwin_amd64.tar.gz"
   version "0.13.0"
-  sha256 "54c9e4c39a5482437f63258abf3836a3b9c738694d558c0f995cf290d4467003"
+  sha256 "3c62bc25261fa033317fc0d04fa3037daf34c1119cd80b6b95b056057fc73bec"
 
   def install
     bin.install "ks"
